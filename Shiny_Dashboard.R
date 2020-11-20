e5836ddcd78a512a6f743a5ba8c1c5067fd4c4ea
herdisbirta <herdisbirta1@gmail.com>
2020-11-20 19:36:45 +0100

    Simple Shiny dashboard
    Shiny dashboard with some tabs and text

---
+++ Shiny_Dashboard.R
@@ -0,0 +1,42 @@
+library(shiny)
+library(shinydashboard)
+
+# Create ui
+ui =
+  dashboardPage(
+    dashboardHeader(
+      title = "Financial Report",
+      titleWidth = 250),
+    dashboardSidebar(
+      width = 250,
+      sidebarMenu(
+        menuItem("Dashboard",
+                 tabName = "dashboard",
+                 icon = icon("dashboard")),
+        menuItem("About",
+                 tabName = "about",
+                 icon = icon("info-circle")))),
+    dashboardBody(
+      fluidRow(
+        
+      tabItems(
+        tabItem(tabName = "dashboard",
+                h4(
+                  " This is the default starting page.
+                  We will put some visualizations here.")),
+        tabItem(tabName = "about",
+                h4(
+                   " Here, we can write some information
+                   about the financial ratios or about how
+                   the dashboard was made."),
+                h5(
+                  "We can also write our names."))))
+    )
+)
+
+# Create server
+server = 
+  function(input,output){}
+
+# Run app
+shinyApp(ui,server)
