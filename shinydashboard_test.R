89a109069f48ca094e178184b1e365bf7da45b8d
livewt <72449349+livewt@users.noreply.github.com>
2020-11-30 00:16:50 +0100

    Add files via upload

--- shinydashboard_test.R
+++ shinydashboard_test.R
@@ -1,10 +1,18 @@
 library(shiny)
 library(shinydashboard)
 library(plotly)
+library(data.table)
 
 # Here, we choose the Telenor file as we run the app
 
 source("Project.R")
+
+# Order given dataset by StandardAccountID and sum ClosingDebitBalance
+SumBySAID <- aggregate(main_df$ClosingDebitBalance,
+                       by=list(StandardAccountID=main_df$StandardAccountID),
+                       FUN=sum)
+SumBySAID$StandardAccountID <- as.numeric(SumBySAID$StandardAccountID) # to compare later,
+                                                                       # easier fix more than welcome
 
 # create dataset
 Art <- c('Salgsinntekt','Annen driftsinntekt',
@@ -22,16 +30,50 @@ Art <- c('Salgsinntekt','Annen driftsinntekt',
             'Skattekostnad på ordinært resultat', 'Ordinært resultat',
             'Ekstraordinære poster', 'Skattekostnad på ekstraordinære poster',
             'Årsresultat')
+
 Resultatregnskap <- 0
 
-art.data <- data.frame(Art, Resultatregnskap)
+art.data <- data.frame(Art, Resultatregnskap, check.names = 'false')
 
-Eiendeler <- c('Anleggsmidler','Omløpsmidler')
-Eiendeler.tall <- 0
-`Egenkapital og Gjeld` <- c('Egenkapital', 'Gjeld')
-EKGJ.tall <- 0
+# EIENDELER
 
-Balanse <- data.frame(Eiendeler, Eiendeler.tall, `Egenkapital og Gjeld`, EKGJ.tall)
+Eiendeler <- c('Immaterielle eiendeler o.l','Tomter, bygninger og annen fast eiendom',
+               'Transportmidler, inventar og maskiner o.l.', 'Finansielle anleggsmidler',
+               'Varelager og forskudd til leverandører', 'Kortsiktige fordringer',
+               'Merverdiavgift, opptjente offentlige tilskudd o.l.', 'Forskuddsbetalt kostnad, påløpt inntekt o.l.',
+               'Kortsiktige finansinvesteringer', 'Bankinnskudd, kontanter og lignende')
+
+StandardAccountID <- c(10,11,12,13,14,15,16,17,18,19)
+
+`Eiendeler tall` <- 0 # placeholder
+
+BalanseEiendeler <- data.frame(Eiendeler, StandardAccountID, `Eiendeler tall`, check.names = 'false')
+
+# Get sum from SumBySAID and insert into BalanseEiendeler
+setDT(BalanseEiendeler)[SumBySAID, `Eiendeler tall` := x, on = .(StandardAccountID)]
+
+BalanseEiendeler$StandardAccountID <- NULL
+
+# EGENKAPITAL OG GJELD
+
+`Egenkapital og Gjeld` <- c('Egenkapital AS/ASA', 'Avsetning for forpliktelser',
+                            'Annen langsiktig gjeld', 'Kortsiktige konvertible lån, obligasjonslån og gjeld til kredittinstitusjoner',
+                            'Leverandørgjeld', 'Betalbar skatt', 'Skattetrekk og andre trekk',
+                            'Skyldige offentlige avgifter', 'Utbytte',
+                            'Annen kortsiktig gjeld')
+
+StandardAccountID <- c(20,21,22,23,24,25,26,27,28,29)
+
+`EKGJ tall` <- 0 # placeholder
+
+BalanseEKGJ <- data.frame(`Egenkapital og Gjeld`, StandardAccountID, `EKGJ tall`, check.names = 'false')
+
+# Get sum from SumBySAID and insert into BalanseEKGJ
+setDT(BalanseEKGJ)[SumBySAID, `EKGJ tall` := x, on = .(StandardAccountID)]
+
+BalanseEKGJ$StandardAccountID <- NULL
+
+
 
 # Create ui
 ui =
@@ -50,7 +92,7 @@ ui =
                  icon = icon("cash-register")),
         menuItem("Balance Statement",
                  tabName = "balancestatement",
-                 icon = icon("cash-register")),
+                 icon = icon("credit-card")),
         menuItem("About",
                  tabName = "about",
                  icon = icon("info-circle")))),
@@ -58,6 +100,13 @@ ui =
     
     dashboardBody(
       fluidRow(
+        tags$head(
+          tags$style(HTML("hr {border-top: 1px solid #000000;}")), # make hr more visible
+          tags$style(HTML("
+            .content{
+               width: 90%;
+            }"))
+        ),
         tabItems(
           tabItem(tabName = "dashboard",
                   tabBox(
@@ -78,12 +127,12 @@ ui =
                     icon = icon("warehouse"),
                     width = 3,
                     color = "purple")),
-        
-          
           tabItem(tabName = "balancestatement",
-                  h4(
+                  h2(
                     "Balance statement"),
-                    tabPanel("Balanse", DT::dataTableOutput("Balanse"))),
+                    tabPanel("BalanseEiendeler", DT::dataTableOutput("BalanseEiendeler")),
+                    hr(),
+                    tabPanel("BalanseEKGJ", DT::dataTableOutput("BalanseEKGJ"))),
           tabItem(tabName = "incomestatement",
                   h4(
                     "Income statement"),
@@ -103,9 +152,11 @@ ui =
 # Create server
 server = 
   function(input,output){
-    
-    output$Balanse <- DT::renderDataTable({
-      DT::datatable(Balanse)
+    output$BalanseEiendeler <- DT::renderDataTable({
+      DT::datatable(BalanseEiendeler)
+    })
+    output$BalanseEKGJ <- DT::renderDataTable({
+      DT::datatable(BalanseEKGJ)
     })
     output$art.data <- DT::renderDataTable({
       DT::datatable(art.data)
