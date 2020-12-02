1ad3687af06dfa2e8591d74f14dc8156cea55141
herdisbirta <herdisbirta1@gmail.com>
2020-12-02 13:53:33 +0000

    Fixed some errors in dashboard

--- shinydashboard_test.R
+++ shinydashboard_test.R
@@ -33,6 +33,107 @@ EKGJ.tall <- 0
 
 Balanse <- data.frame(Eiendeler, Eiendeler.tall, `Egenkapital og Gjeld`, EKGJ.tall)
 
+# Order given dataset by StandardAccountID and sum ClosingDebitBalance
+SumBySAID <- aggregate(main_df$ClosingDebitBalance,
+                       by=list(StandardAccountID=main_df$StandardAccountID),
+                       FUN=sum)
+SumBySAID$StandardAccountID <- as.numeric(SumBySAID$StandardAccountID) # to compare later,
+                                                                       # easier fix more than welcome
+
+SumByAID <- aggregate(main_df$ClosingDebitBalance,
+                      by=list(AccountID=main_df$AccountID),
+                      FUN=sum)
+SumByAID$AccountID <- as.numeric(SumByAID$AccountID)
+
+# --------------------
+# Resultatregnskap etter art
+# --------------------
+
+`Resultatregnskap etter art` <- c('Salgsinntekt', 'Varekostnad', 'Lønnskostnad',
+                                  'Avskrivning', 'Nedskrivning',
+                                  'Annen driftskostnad', 'Finansinntekt',
+                                  'Finanskostnad',
+                                  'Skattekostnad på ordinært resultat',
+                                  'Ekstraordinær inntekt', 
+                                  'Ekstraordinær kostnad', 
+                                  'SKattekostnad på ekstraordinært resultat',
+                                  'Årsresultat', 'Overføringer/disponeringer')
+
+#AccountIDRange <- c(Salgsinntekt = 3000:3970, Varekostnad = 4000:4990, Lønnskostnad = 5000:5930,
+#                    Avskrivning = 6000:6020, Nedskrivning = 6050, `Annen driftskostnad` = 6100:7910,
+#                    Finansinntekt = 8000:8080, Finanskostnad = 8100:8170, 
+#                    `Skattekostnad på ordinært resultat` = 8300:8320,
+#                    `Ekstraordinær inntekt` = 8400, `Ekstraordinær kostnad` = 8500,
+#                    `SKattekostnad på ekstraordinært resultat` = 8600:8620,
+#                    Årsresultat = 8800, `Overføringer/disponeringer` = 8900:8990)
+
+#match(names(AccountIDRange), `Resultatregnskap etter art`)
+
+#`REA Tall` <- 0 # placeholder
+
+# Get sum from SumByAID and insert into REA
+#setDT(AccountIDRange)[SumByAID, `REA Tall` := x, on = .(AccountIDRange)]
+
+#BalanseREA <- data.frame(`Resultatregnskap etter art`, AccountIDRange, 
+#                       `REA Tall`, check.names = 'false')
+
+
+# --------------------
+# EIENDELER
+# --------------------
+
+Eiendeler <- c('Immaterielle eiendeler o.l',
+               'Tomter, bygninger og annen fast eiendom',
+               'Transportmidler, inventar og maskiner o.l.', 
+               'Finansielle anleggsmidler',
+               'Varelager og forskudd til leverandører', 
+               'Kortsiktige fordringer',
+               'Merverdiavgift, opptjente offentlige tilskudd o.l.', 
+               'Forskuddsbetalt kostnad, påløpt inntekt o.l.',
+               'Kortsiktige finansinvesteringer', 
+               'Bankinnskudd, kontanter og lignende')
+
+StandardAccountID <- c(10:19)
+
+`Eiendeler tall` <- 0 # placeholder
+
+BalanseEiendeler <- data.frame(Eiendeler, StandardAccountID, 
+                               `Eiendeler tall`, check.names = 'false')
+
+# Get sum from SumBySAID and insert into BalanseEiendeler
+setDT(BalanseEiendeler)[SumBySAID, `Eiendeler tall` := x, on = .(StandardAccountID)]
+
+# Remove SAID from BalanseEiendeler
+BalanseEiendeler$StandardAccountID <- NULL
+
+
+# --------------------
+# EGENKAPITAL OG GJELD
+# --------------------
+
+`Egenkapital og Gjeld` <- c('Egenkapital AS/ASA', 'Avsetning for forpliktelser',
+                            'Annen langsiktig gjeld', 
+                            'Kortsiktige konvertible lån, obligasjonslån og gjeld til kredittinstitusjoner',
+                            'Leverandørgjeld', 'Betalbar skatt', 
+                            'Skattetrekk og andre trekk',
+                            'Skyldige offentlige avgifter', 'Utbytte',
+                            'Annen kortsiktig gjeld')
+
+StandardAccountID <- c(20:29)
+
+`EKGJ tall` <- 0 # placeholder
+
+BalanseEKGJ <- data.frame(`Egenkapital og Gjeld`, StandardAccountID, 
+                          `EKGJ tall`, check.names = 'false')
+
+# Get sum from SumBySAID and insert into BalanseEKGJ
+setDT(BalanseEKGJ)[SumBySAID, `EKGJ tall` := x, on = .(StandardAccountID)]
+
+# Remove SAID from BalanseEKGJ
+BalanseEKGJ$StandardAccountID <- NULL
+
+
+
 # Create ui
 ui =
   dashboardPage(
@@ -119,7 +220,6 @@ ui =
                     title = "Wages to Salary Ratio",
                     status = "warning",
                     solidHeader = TRUE,
-                    height = 275,
                     renderPlot(
                       w_to_s_chart))),
           tabItem(tabName = "transactions",
@@ -127,7 +227,6 @@ ui =
                     title = "Transactions",
                     status = "primary",
                     solidHeader = TRUE,
-                    height = 275
 #                    ,*plot name here*
                   )),
         
@@ -149,7 +248,7 @@ ui =
                   h5(
                     "We can also write our names.")))
         
-    )
+      )
   )
 )
 
